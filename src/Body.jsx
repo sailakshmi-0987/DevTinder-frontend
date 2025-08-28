44a5f3a04648a1b78bc580f157b6fe0d10ce2339
Sailakshmi-0987 <gajulapallisailakshmi@gmail.com>
2025-08-28 19:26:36 +0530

    Updated backend url

--- src/Body.jsx
+++ src/Body.jsx
@@ -6,6 +6,7 @@ import { BASE_URL } from './utils/constants';
 import axios from 'axios';
 import { useDispatch, useSelector } from 'react-redux';
 import { addUser } from './utils/userSlice';
+
 const Body = () => {
   
   const dispatch = useDispatch();
--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,2 +1,4 @@
-export const BASE_URL = "http://localhost:3000";
+export const BASE_URL = import.meta.env.VITE_API_URL || "http://localhost:3000";
+// for CRA: process.env.REACT_APP_API_URL
+
 
